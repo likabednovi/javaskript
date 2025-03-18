505e00d1d7889eeda72083c6893c8cf3644c4e9a
likabednovi <likabednovi@gmail.com>
2025-03-18 12:14:46 +0400

    javaskript

---
+++ script/script.js
@@ -0,0 +1,381 @@
+// var firstName = "lika"
+// console.log(firstName)
+
+// let saxeli = prompt("შეიყვანე შენი სახელი");
+// alert(`გამარჯობა ${saxeli}`);
+// let gvari = prompt("შეიყვანე შენი გვარი");
+// alert(`შენი გვარი ${gvari}`);
+// let asaki = prompt("შეიყვანე შენი ასაკი");
+// alert(`შენი ასაკი ${asaki}`);
+
+
+// alert(`გამარჯობა ${saxeli} ${gvari} მიხარია რომ უკვე ${2024 - Number(asaki)} წლის ხარ`);
+// alert(`გამარჯობა ${saxeli} ${gvari} თქვენ ხართ  ${2024 - Number(asaki)} წელს დაბადებული`)
+// let color = ["Blue", "Green", "Red", "Orange", "Violet", "Indiogo", "Yellow",];
+// let size = color.length;
+// console.log(color);
+// console.log(Array.isArray(color));
+// let firstColor = color.at(0);
+// console.log(firstColor);
+// let lastColor = color.at(-1);
+// console.log(lastColor);
+// color.unshift("pink");
+// console.log(color);
+// color.push("gray");
+// console.log(color);
+// color[2] = "black";
+// console.log(color);
+// let sliseColor = color.slice(2, 6);
+// console.log(sliseColor);
+// let spliseColor = color.splice(2,6)
+// console.log(spliseColor)
+
+
+// let bag = ["balensiga", "guchi", "bagstace"];
+// console.log(bag);
+// let conspi = color.concat(bag,spliseColor);
+// console.log(conspi);
+// let overall=(conspi.join("_"));
+// console.log(overall)
+// console.log(typeof overall)
+
+
+
+
+
+
+
+
+
+
+
+
+// let carText = cars.join("_")
+// console.log(carText);
+// console.log(typeof cars)
+// console.log(size)
+// console.log(cars.at(3))
+// console.log(typeof size)
+// console.log(typeof carText)
+// console.log(cars.at(4));
+//  console.log(cars.join(" _ "));
+//  console.log()
+
+// let city = ["tbilisi", "borjomi", "qutaisi", "zestafoni", "mcxeta"];
+// let size = city.length;
+// console.log(city.at(1));
+// console.log(city);
+// let cityText = city.join("");
+// console.log(typeof city);
+// console.log(city.join("_"));
+// console.log(typeof cityText);
+
+// let phone= ["iphone", "android", "huavei", "poko", "google", "iphone", "android", "huavei", "poko", "google" ]
+// let size= phone.length
+// console.log(phone.at(3))
+// let phonetext = phone.join("_")
+// console.log(phone)
+// console.log(typeof phone)
+// console.log(size)
+// console.log(typeof phonetext)
+
+// let phone= ["iphone", "android", "huavei", "poko", "google", "iphone", "android", "huavei", "poko", "google" ];
+// let size = phone.length;
+// console.log(phone);
+// console.log(size);
+// phone.shift();
+// phone.unshift("ipad");
+// phone.pop();
+// phone.push("macbook");
+// console.log(phone);
+// console.log(size);
+// phone[6] = "listeting";
+// console.log( phone);
+// console.log(size);
+// console.log(phone.at(3));
+// let phonePhone = phone.join("_");
+// console.log(phonePhone);
+// console.log(typeof phonePhone);
+// console.log(size);
+// delete phone[3]
+// console.log(phone)
+// console.log(size)
+// let cars=["bmw","mersedes","porcshe"]
+// console.log(cars)
+// let car="volvo"
+// let carSize = cars.length
+// console.log(carSize)
+// console.log(Array.isArray(color));
+
+
+
+
+// let number=[1,2 ,3,4,5,[6,7, [8,9, [10,11],]]];
+// console.log(number);
+// let lastNamber = number.length-1;
+// console.log(lastNamber);
+// console.log(lastNamberindex);
+// console.log(number.at(lastNamberindex).at(2).at(1));
+
+// 
+
+// let number2 = "15";
+// let number3 = 3;
+
+
+// let number5 = number2 + number3;
+// let number55 = Number (number2) + number3;
+// let number6 = number2 - number3;
+// let number7 = number2 * number3;
+// let number8 = number2 / number3;
+// let number9 = number2 % number3;
+
+
+// console.log(typeof number5)
+// console.log(number5)
+// console.log(number55)
+// console.log(typeof number6)
+// console.log(number6)
+// console.log(typeof number7)
+// console.log(number7)
+// console.log(typeof number8)
+// console.log(number8)
+// console.log(typeof number9)
+// console.log(number9)
+
+
+// let number10 = number2 + number3;
+// console.log(typeof number10)
+// console.log(number10)
+
+// let number2="1"
+// let number3=1
+
+// let shedarebebi = number2 & number3
+// console.log(shedarebebi)
+
+// && ||
+// && gamoaqvs pirveli araswori Elementi || gamoaqvs pirveli swori eleneti
+
+//  let number4=1 && 0 && 2 && 3
+//  console.log(number4)
+// let number5= 0 || 0 || 2 || 3
+// console.log(number5)
+
+
+// let number2 = prompt("შეიყვანე პირველი რიცხვი a")
+// let number3 = prompt("შეიყვანე მეორე რცხვი b")
+// let number4 = prompt("შეიყვანე მესამე რიცხი c")
+// let number5 = (Number(number2) + Number(number3) + Number(number4)) / 3
+// console.log(number5)
+// let number6 = number2 % number3
+// console.log(number6)
+
+// let number1= prompt("პირველის კუთხე");
+// let number2= prompt("მეორე კუთხე");
+// let number3=(Number(number1) + Number(number2))
+// console.log(number3)
+
+
+
+
+
+
+// let asaki = prompt("მერამდენე კლასში ხარ");
+
+// if (asaki > 1 && asaki <= 6) {
+//     console.log("შენ ხარ დაწყებით საფეხურზე")
+// } else if (asaki > 7 && asaki <= 10) {
+//     console.log("შენ ხარ საბაზო საფეხურზე")
+// } else if (asaki > 10 && asaki <= 12) {
+//     console.log("შენ ხარ საშუალო საფეხურზე")
+// } else {
+//     console.log("შენ არ დადიხარ სკოლაში")
+// }
+
+
+// let num1=prompt("რიცხვი1")
+// num1=Number(num1)
+// console.log(num1)
+// let num2=prompt("რიცხვი2")
+// num2=Number(num2)
+// console.log(num2)
+// let num3=prompt("რიცხვი3")
+// num3=Number(num3)
+// console.log(num3)
+
+// if( (num1>=num2) && (num1>=num3)){
+//     alert(`${num1} არის ყველაზე დიდი`)
+// } else if(num2>num1 && num2>num3){
+//     alert(`${num2} არის ყველაზე დიდი`)
+// }else if(num3>num1 && num3>num2)(
+//  alert(`${num3} არის ყველაზე დიდი`)
+// )
+
+// let x="დაწყებითი";
+// let y="საბაზო";
+// let z="საშუალო"
+
+
+
+
+// switch("საშუალო") {
+//     case "საბაზო":
+//         console.log("შენ ხარ 10 დან მე 12 კლასის ჩათვლით მოსწავლე")
+//         break;
+//         case "საშუალო":
+//             console.log("შენ ხარ მე 7 დედან მე9 კლასის ჩათვლით მოსწავლე")
+//             break;
+//             case "დაწყებითი":
+//                 console.log("შენ ხარ პირველიდან მე 6 კლასის ჩათლით მოსწავლე")
+//                 break;
+// }
+
+
+// let qula = prompt("შეიყვანე მიღებული ქულა")
+
+
+// if (qula > 90 && qula < 101) {
+//     alert(`შენ მიიღე ${qula} ქულა რომელიც არის A შეფასება`)
+// } else if (qula > 80 && qula < 91) {
+//     alert(`შენ მიიღე ${qula} ქულა რომელიც არის  B შეფასება`)
+// } else if (qula > 70 && qula < 81) {
+//     alert(`შენ მიიღე ${qula} ქულა რომელივ არის C შეფასენა`)
+// } else if (qula > 60 && qula < 71) {
+//     alert(`შენ მიიღე ${qula} ქულა რომელიც არის D შეფასება`)
+// } else if (qula > 49 && qula < 61) {
+//     alert(`შენ მიიღე ${qula} ქულა რომელიც არის E შეფასება`)
+// } else if(qula>0 && qula<50){
+//     alert(`შენ ვერ ჩააბარე გამოცდა`)
+// } else {
+//     alert(`გთხოვთ ჩაწეროთ სწორი შეფასება `)
+// }
+
+
+
+
+//  let qula= prompt("შეიყვანე მიღებული შეფასება")
+
+// switch (qula){
+//     case "A":
+//         console.log("შენ მიიღე 91-100 მდე")
+//         break;
+//     case "B":
+//     console.log("შენ მიიღე 81-90 მდე")
+//     break;
+//     case "C":
+//         console.log("შენ მიიღე 71-80 მდე")
+//         break;
+//         case "D":
+//             console.log("შენ მიიღე 61-70 მდე")
+//             break;
+//             case "E":
+//                 console.log("შენ მიიღე 50-60 მდე")
+//                 break;
+//                 default:
+//                     console.log("ჩაწერე სწორი შეფასება")
+// }
+
+
+
+
+
+
+// let tve = prompt("შეივანე თვის დასახელება")
+
+// switch (tve) {
+//     case "იანვარი":
+//     case "თებერვალი":
+//     case "დეკემბერი":
+//         console.log(`${tve} არის ზამთარი`)
+//         break;
+//     case "მარტი":
+//     case "აპრილი":
+//     case "მაისი":
+//         console.log(`${tve} არის გაზაფხული`)
+//         break;
+//     case "ივნისი":
+//     case "ივლისი":
+//     case "აგვისთო":
+
+//         console.log(`${tve} არის ზაფხული`)
+//         break;
+//     case "სექტემბერი":
+//     case "ოქტომბერი":
+//     case "ნოემბერი":
+//         console.log(`${tve}არის შემოდგომა`)
+//         break;
+//     default:
+//         console.log("ჩაწერე სწორად")
+
+// }
+
+
+
+
+
+
+// let i = 0
+
+// while(i<100){
+//     console.log(i)
+//     i=i+2
+// }
+
+
+// let a = prompt("შეიყვანე პირველი რიცხვი")
+// let b = prompt("შეიყვანე მეორე რიცხვი")
+
+// for ( let i = Number(a); i<100; i=i+Number(b)){
+// console.log(i)
+// }
+
+// const box = document.querySelector(".box")
+// const texth1 = document.querySelector(".texth1")
+// console.dir(texth1)
+// console.dir(box)
+// console.log(box)
+// box.style.width="500px"
+// box.style.heigth="300px"
+// box.style.backgroundColor="pink"
+
+// texth1.innertext="hi"
+// texth1.style.color= "black"
+// texth1.style.fontSize="20px"
+
+// const texth1 = document.querySelector(".texth1")
+// console.dir(texth1)
+
+// texth1.innerText="პრივეტ"
+// texth1.style.color= "pink"
+// texth1.style.fontSize="50px"
+
+// texth1.onclick = function(){
+//     texth1.innerText="გამარჯობა!"
+//     texth1.style.color ="red"
+// }
+
+
+const bill = document.querySelector(".calcuratorshi_div_div")
+const amount = document.querySelector(".meore_div_h4")
+const total =document.querySelector(".meore_div_h4_1")
+const num = document.querySelector(".nam")
+
+console.dir(amount)
+
+console.dir(bill)
+
+function func5(a){
+    let y= bill.value;
+    let x = (y * a) / 100;
+    let z = num.value;
+    amount.innerText = `$ ${x}`;
+    total.innerText= `$ ${z * x}`;
+}
+
+function reset(){
+    bill.value = ""
+    total.innerText=" $ 0.00"
+    amount.innerText="$ 0.00"
+    num.value =""
+}
